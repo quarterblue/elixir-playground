22bdadf20aec6b548a8f389ab7d4fe032c9b3a09
quarterblue <quarterslate@gmail.com>
2021-02-26 22:46:02 -0800

    Initialize card shuffler project

---
+++ cards/lib/cards.ex
@@ -0,0 +1,12 @@
+defmodule Cards do
+
+  def create_deck do
+    ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"] 
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+
+end
